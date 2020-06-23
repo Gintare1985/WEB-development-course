4b0fc74d479584dced5b6ae4ee8ce3b95bca126a
Gintare1985 <g.bulbuke@gmail.com>
2020-06-23 18:07:08 +0300

    Homework JS4, initial work.

---
+++ Homework/JS4/js/scripts.js
@@ -0,0 +1,20 @@
+function getInputValueFromElementById(idName) {
+var inputObject = document.getElementById (idName);
+var inputValue = inputObject.value;
+	return inputValue;
+}
+
+var firstNumber = getInputValueFromElementById("first-number");
+var secondNumber = getInputValueFromElementById("second-number");
+
+
+
+function divideTwoNumbers (firstNumber, secondNumber) {
+	var result = firstNumber / secondNumber;
+	return result;
+}
+
+
+function getResultToConsole (result) {
+	console.log(result);
+}
